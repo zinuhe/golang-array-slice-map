78ad299b0f2bf310d0c30a77d4346b632bc1b475
Jimmy <11039182+zinuhe@users.noreply.github.com>
2021-10-26 17:39:18 -0400

    Create multidimensional.go

---
+++ array/multidimensional.go
@@ -0,0 +1,17 @@
+package array
+
+import "fmt"
+
+/*
+func main() {
+	var a [2][3]string
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			a[i][j] = fmt.Sprintf("row %d - column %d", i+1, j+1)
+		}
+	}
+	fmt.Printf("%q", a)
+	// [["row 1 - column 1" "row 1 - column 2" "row 1 - column 3"]
+	//  ["row 2 - column 1" "row 2 - column 2" "row 2 - column 3"]]
+}
+*/
