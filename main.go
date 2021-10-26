6025435808fb98d6bc65c3224bc357eab0f41792
Jimmy <zinuhe@outlook.com>
2021-10-25 23:57:04 -0400

    added main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	array "github.com/zinuhe/golang-array-slide-map/array"
+	slide "github.com/zinuhe/golang-array-slide-map/slide"
+)
+
+func main() {
+	fmt.Println("==============================ARRAYS==============================")
+	array.Declaration()
+	array.Iterator()
+
+	fmt.Println("==============================SLIDES==============================")
+	slide.Declaration()
+	slide.Iterator()
+
+	fmt.Println("===============================MAPS===============================")
+
+}
