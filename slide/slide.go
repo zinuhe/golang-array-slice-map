3cce24ac6efef63c84782baf232cbf18989b0720
Jimmy <zinuhe@outlook.com>
2021-10-28 20:57:33 -0400

    Fix typo and add prints

--- slide/slide.go
+++ slide/slide.go
@@ -4,32 +4,33 @@ import (
 	"fmt"
 )
 
-var intSlide = []int{1, 2, 3, 4}
-var strSlide = []string{"one", "two", "three", "four"}
+var intSlice = []int{1, 2, 3, 4}
+var sliceStrings = []string{"one", "two", "three", "four"}
 
 func Declaration() {
-	fmt.Println("\nSlide Declaration")
+	fmt.Println("\nSlice Declaration")
 
-	boolSlide := []bool{true, false, true}
+	// boolSlide := []bool{true, false, true}
 
-	// fmt.Println("intSlide:", intSlide)
-	fmt.Printf("intSlide: %T -- %v \n", intSlide, intSlide)
+	fmt.Println("sliceStrings:", sliceStrings)
 
-	fmt.Printf("strSlide: %T -- %v \n", strSlide, strSlide)
-
-	fmt.Printf("boolSlide: %T -- %v \n", boolSlide, boolSlide)
+	fmt.Printf("%T\n",  sliceStrings)
+	fmt.Printf("%v\n",  sliceStrings)
+	fmt.Printf("%+v\n", sliceStrings)
+	fmt.Printf("%#v\n", sliceStrings)
+	fmt.Printf("%+q\n", sliceStrings)
 }
 
 func Iterator() {
-	fmt.Println("Slide Iterator")
+	fmt.Println("\nSlice Iterator")
 
-	fmt.Println("\nfor")
-	for i := 0; i < len(intSlide); i++ {
-		fmt.Println("i:", i, "val:", intSlide[i])
+	fmt.Println("for")
+	for i := 0; i < len(intSlice); i++ {
+		fmt.Println("i:", i, "val:", intSlice[i])
 	}
 
 	fmt.Println("\nfor range")
-	for i, v := range intSlide {
+	for i, v := range intSlice {
 		fmt.Println("i:", i, "val:", v)
 	}
 
