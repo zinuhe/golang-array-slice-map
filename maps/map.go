ededa2304b970ea786c83e45dc67f8ea1f1aee7c
Jimmy <11039182+zinuhe@users.noreply.github.com>
2021-11-03 21:13:09 -0400

    Update map.go

--- maps/map.go
+++ maps/map.go
@@ -7,3 +7,8 @@ import (
 func Declaration() {
     fmt.Println("Map Declaration")
 }
+
+
+//Real Implementation
+//r: map[_shards:map[failed:0 skipped:0 successful:0 total:0] hits:map[hits:[] max_score:0 total:map[relation:eq value:0]] timed_out:false took:2]
+//r[hits]: map[hits:[] max_score:0 total:map[relation:eq value:0]]
